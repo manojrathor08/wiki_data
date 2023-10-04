57bb5f512f7fe77d054273875402beb657e222d5
Manoj Rathor <rathor.manoj08@gmail.com>
2023-10-04 22:11:18 +0900

    Created run_wiki.py

---
+++ run_wiki.py
@@ -0,0 +1,13 @@
+import requests
+
+# Define the URL for FastAPI application
+url = "http://localhost:8001/wiki/"
+
+# Prompt the user to enter a Eikipedia title and length of the summary
+title = input("Enter a wikipedia title:")
+lenght = input("Enter te summary length (defual is 10):")
+
+# Define the query parameters
+params = {'title': title, 'length':length}
+# Make the GET request with the query parameters
+repsone = requests.get(url,params = params)
